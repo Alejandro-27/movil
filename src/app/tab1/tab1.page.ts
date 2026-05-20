9afe2dfc548a1d045d6532c458fdc821ff5932c3
Alejandro_g27 <cesaralejandrog67@gmail.com>
2026-05-20 15:12:23 -0500

    Aprendiendo desarrolo movil

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Component } from "@angular/core";
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonCheckbox,
+} from "@ionic/angular/standalone";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
+  standalone: true,
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    CommonModule,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonCheckbox,
+  ],
 })
 export class Tab1Page {
+  tareas = [
+    { nombre: "Configurar pnpm en Debian Trixie", completada: true },
+    { nombre: "Levantar el servidor con ionic serve", completada: true },
+    { nombre: "Aprender directivas Standalone", completada: false },
+    { nombre: "Compilar el APK final", completada: false },
+  ];
   constructor() {}
+
+  cambiarEstado(index: number) {
+    this.tareas[index].completada = !this.tareas[index].completada;
+  }
 }
